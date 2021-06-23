bd4b978335896b8963aae1c702bf3b2285deda3d
ruslanbobetsky <ruslanbobetsky@gmail.com>
2021-06-24 01:09:14 +0300

    processing

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
-$(function(){
+// $(function(){
 
-	new WOW().init();
+// 	new WOW().init();
+// });
+
+const swiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	direction: 'horizontal',
+	//loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	  },
+	  autoplay: { 
+        delay: 3500, 
+      }, 
+      slidesPerView: 'auto',      
+	  scrollbar: {
+		el: '.swiper-scrollbar',
+	  },
 });
